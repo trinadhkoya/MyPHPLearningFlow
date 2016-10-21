999d02fceabc75efa20fbb492fa9644f3c36f1b2
androidmates <trinadhkoya@outlook.com>
2016-10-21 14:42:15 +0530

    init
    Database connection through PDO

---
+++ mysql_connect_pdo.php
@@ -0,0 +1,32 @@
+	<?php
+
+
+	//this is a way to connect mysql/whatever through PDO (php data object)
+	//you can see more on PDO over MySQL or MySQLi from this link
+	//https://code.tutsplus.com/tutorials/pdo-vs-mysqli-which-should-you-use--net-24059
+
+	$servername='localhost';
+	$username='root';
+	$password='';
+	$dbname='my_first_database';
+
+
+
+
+	try {
+		//creating connection 
+	$connection=new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+
+	// benefit of PDO is that it has an exception class to handle any problems
+	$connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	echo "connection success";
+
+	} catch (Exception $e) {
+		echo "connection failed ".$e->getMessage();
+	}
+
+
+
+
+
+	?>
